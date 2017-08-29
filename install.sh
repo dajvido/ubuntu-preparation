83ab4f6f7f75d23d9a8d3fcf0cbc124082d7d5c6
Dawid Jaskot <dawid.jaskot@gmail.com>
2017-08-29 21:19:56 +0100

    Add vim install plugin

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/dajvido/ubuntu-preparation
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/dajvido/ubuntu-preparation/master/copyq-install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/dajvido/ubuntu-preparation/master/docker-install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/dajvido/ubuntu-preparation/master/nvm-install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/dajvido/ubuntu-preparation/master/vim-install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/dajvido/ubuntu-preparation/master/xcape-install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/dajvido/ubuntu-preparation/master/zsh-install.sh)"
 cd $HOME
---
+++ vim-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Started installation of vim"
+echo "Install vim"
+sudo apt-get install vim
+echo "Succeed installation of vim"
