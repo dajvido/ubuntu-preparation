ce671827a2b6590ef1b9e305e716fc081f0e5087
Dawid Jaskot <dawid.jaskot@gmail.com>
2017-08-29 21:54:12 +0100

    Refactor initial script for asdf

--- asdf-install.sh
+++ asdf-install.sh
@@ -7,6 +7,7 @@ echo "Install dependencies"
 sudo apt-get install -y automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
 echo "Clone & install asdf"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
-echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+echo -e '\n# Initial asdf on shell initial
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
 echo "Succeed installation of asdf"
