3b44e52ae92d5f11c23870698ce05d3d136f34a1
Dawid Jaskot <dawid.jaskot@gmail.com>
2017-08-29 21:51:43 +0100

    Set vim as default editor

--- vim-install.sh
+++ vim-install.sh
@@ -3,4 +3,8 @@
 echo "Started installation of vim"
 echo "Install vim"
 sudo apt-get install vim
+echo "Set vim as default editor"
+echo -e '\n# Set vim as default editor
+export EDITOR=vim
+export VISUAL=vim' >> ~/.zshrc
 echo "Succeed installation of vim"
