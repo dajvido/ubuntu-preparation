1f0d17510452277de143306f39fb7cc91d7b8fe1
Dawid Jaskot <dawid.jaskot@gmail.com>
2017-08-30 23:15:08 +0100

    Update vim installation script

--- vim-install.sh
+++ vim-install.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Started installation of vim"
-echo "Install vim"
-sudo apt-get install vim
+echo "Install dependencies"
+sudo apt-get install -y liblua5.1-dev luajit libluajit-5.1
+echo "Clone vim"
+git clone https://github.com/vim/vim.git vim
+cd vim/src
+./configure --enable-gui=auto --enable-gtk2-check --enable-luainterp --with-luajit --with-x --prefix=/usr --with-features=big
+echo "Make and install"
+make
+sudo make install
+cd ../..
 echo "Set vim as default editor"
 echo -e '\n# Set vim as default editor
 export EDITOR=vim
